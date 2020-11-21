fff9e60849a6de314bd1e6d74ad0aa0619f223f5
Rebecca Howe <Rebeccahowe20@gmail.com>
2020-11-21 15:17:51 -0800

    move if statements and else statements

--- script.js
+++ script.js
@@ -16,63 +16,73 @@ function writePassword() {
 }
 
 function generatePassword(){
-  var length = (prompt('How long do you want your password?'));
-  console.log(length)
-  var numbers = confirm('Would you like numbers?');
-  console.log(numbers)
-  var lettersUpper = confirm('Would you like Uppercase letters?');
-  console.log(lettersUpper)
-  var lettersLower = confirm('Would you like Lowercase letters?');
-  console.log(lettersLower)
-  var chars = confirm('Would you like Special Characters?');
-  console.log(chars)
-  
+
   var charChoices = [];
   console.log(charChoices)
   var passwordArr = [];
   console.log(passwordArr)
 
+  var length = parseInt(prompt('How long do you want your password?'));
   if(length >= 8 && length <= 128){
-    charChoices.push('length')
+    console.log(length)
 
-    if(numbers === true){
-      charChoices.push('numbers')
-    }
-    if(lettersUpper === true){
-      charChoices.push('lettersUpper')
-    }
-    if(lettersLower === true){
-      charChoices.push('lettersLower')
-    }
-    if(chars === true){
-      charChoices.push('chars')
-    }
-    console.log(charChoices)
+  var numbers = confirm('Would you like numbers?');
+  if(numbers === true){
+    charChoices.push('numbers')
+  }
+  console.log(numbers)
+
+  var lettersUpper = confirm('Would you like Uppercase letters?');
+  if(lettersUpper === true){
+    charChoices.push('lettersUpper')
+  }
+  console.log(lettersUpper)
+
+  var lettersLower = confirm('Would you like Lowercase letters?');
+  if(lettersLower === true){
+    charChoices.push('lettersLower')
+  }
+  console.log(lettersLower)
+
+  var chars = confirm('Would you like Special Characters?');
+  if(chars === true){
+    charChoices.push('chars')
+  }
+  console.log(chars)
+}else{
+  alert('Please choose amount between 8 and 128!')
+}
+console.log(charChoices)
+
     for(var i = 0; i < length; i++){
       var randomCharChoice = charChoices[Math.floor(Math.random() * charChoices.length)-1]
-      console.log(randomCharChoice)
 
       if(randomCharChoice === 'numbers'){
+      }else{
         var randomNum = Math.floor(Math.random() * Math.floor(9))
         passwordArr.push(randomNum)
       }
       if(randomCharChoice === 'lettersUpper'){
+      }else{
         var randomUpper = upperCase[Math.floor(Math.random() * upperCase.length)-1]
         passwordArr.push(randomUpper)
       }
       if(randomCharChoice === 'lettersLower'){
+      }else{
         var randomLower = lowerCase[Math.floor(Math.random() * lowerCase.length)-1]
         passwordArr.push(randomLower)
       }
       if(randomCharChoice === 'chars'){
+      }else{
         var randomSpecialChar = specialChar[Math.floor(Math.random() * specialChar.length)-1]
         passwordArr.push(randomSpecialChar)
+        console.log(specialChar)
       }
     }
     var passwordString = passwordArr.join('');
     return passwordString
   }
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
