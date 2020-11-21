32dc6e31b2416afb191be8aa46d9e115ba188ba6
Rebecca Howe <Rebeccahowe20@gmail.com>
2020-11-21 11:24:02 -0800

    updated function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");{
 
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
- var specialChar = ['!','@','#','$','%','^','&','*','?','(',')','<','>','=','+','-'];
+var specialChar = ['!','@','#','$','%','^','&','*','?','(',')','<','>','=','+','-'];
 
 
 // Write password to the #password input
@@ -16,7 +16,7 @@ function writePassword() {
 }
 
 function generatePassword(){
-  var length = parseInt(prompt('How long do you want your password?'));
+  var length = (prompt('How long do you want your password?'));
   console.log(length)
   var numbers = confirm('Would you like numbers?');
   console.log(numbers)
@@ -26,12 +26,15 @@ function generatePassword(){
   console.log(lettersLower)
   var chars = confirm('Would you like Special Characters?');
   console.log(chars)
+  
   var charChoices = [];
   console.log(charChoices)
   var passwordArr = [];
   console.log(passwordArr)
 
   if(length >= 8 && length <= 128){
+    charChoices.push('length')
+
     if(numbers === true){
       charChoices.push('numbers')
     }
@@ -44,34 +47,32 @@ function generatePassword(){
     if(chars === true){
       charChoices.push('chars')
     }
-
+    console.log(charChoices)
     for(var i = 0; i < length; i++){
-      var randomcharchoice = charChoices[Math.floor(Math.random() * charChoices.length)-1]
-      console.log(randomcharchoice)
+      var randomCharChoice = charChoices[Math.floor(Math.random() * charChoices.length)-1]
+      console.log(randomCharChoice)
 
-      if(randomcharchoice === 'numbers'){
-        var randomNum = Math.floor(Math.random() * Math.floor(10))
+      if(randomCharChoice === 'numbers'){
+        var randomNum = Math.floor(Math.random() * Math.floor(9))
         passwordArr.push(randomNum)
       }
-      if(randomcharchoice === 'lettersUpper'){
+      if(randomCharChoice === 'lettersUpper'){
         var randomUpper = upperCase[Math.floor(Math.random() * upperCase.length)-1]
         passwordArr.push(randomUpper)
       }
-      if(randomcharchoice === 'lettersLower'){
+      if(randomCharChoice === 'lettersLower'){
         var randomLower = lowerCase[Math.floor(Math.random() * lowerCase.length)-1]
         passwordArr.push(randomLower)
       }
-      if(randomcharchoice === 'chars'){
+      if(randomCharChoice === 'chars'){
         var randomSpecialChar = specialChar[Math.floor(Math.random() * specialChar.length)-1]
         passwordArr.push(randomSpecialChar)
       }
     }
     var passwordString = passwordArr.join('');
     return passwordString
-  }else{
-    alert('Please choice between 8 and 128')
   }
-}console.log(alert)
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
